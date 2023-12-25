267f332079df5a7c237afc6e9cf97057065f9b1a
abu in vm <ubuntu2204@example.com>
2023-12-25 14:30:24 +0800

    Exclude .idea directory from tracking

--- flask_search.py
+++ flask_search.py
@@ -17,6 +17,16 @@ def get_db_connection():
     conn = mysql.connector.connect(**config)
     return conn
 
+@app.route('/result')
+def index():
+    results = [
+        {"url": "https://free.com.tw/diffchecker/", "title": "new stuff - 在線文本對比工具", "description": "使用 DiffChecker，您可以快速比較兩段文本的差異。"},
+        {"url": "https://free.com.tw/diffnow/", "title": "DiffNow - 立即比較文本、文件和目錄", "description": "DiffNow 允許您比較內容，包括文本和文件夾，簡單易用。"},
+        # 添加更多資料，直到達到 10 筆
+        # ...
+    ]
+    return render_template('search_result.html', results=results)
+
 @app.route('/')
 def job_listing():
     conn = get_db_connection()
@@ -24,16 +34,15 @@ def job_listing():
 
     # 假設您的表名為 'jobs'，並且有相應的列
  
-    # try:
+    try:
         # 假設您的表名為 'jobs'，並且有相應的列
-    cursor.execute("SELECT title, company_name, job_description, salary_range, address, other_info FROM jobs;")
-    job_data = cursor.fetchone()
-    print(job_data)
-    # finally:
-    cursor.close()
-    conn.close()
-
+        cursor.execute("SELECT title, company_name, job_description, salary_range, address, other_info, url FROM jobs limit 1 offset 2;")
+        job_data = cursor.fetchone()
+        print(job_data)
+    finally:
+        cursor.close()
+        conn.close()
     return render_template('job_listing.html', job=job_data)
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=80, debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True)
